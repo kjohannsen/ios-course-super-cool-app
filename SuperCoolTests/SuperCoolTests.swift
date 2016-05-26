5313557103590257dc4771de4f90e1b587e2079a
Kyle Johannsen <kylejohannsen@kyles-air.attlocal.net>
2016-05-26 12:41:43 -0400

    Initial Commit

---
+++ SuperCoolTests/SuperCoolTests.swift
@@ -0,0 +1,36 @@
+//
+//  SuperCoolTests.swift
+//  SuperCoolTests
+//
+//  Created by Kyle Johannsen on 5/26/16.
+//  Copyright (c) 2016 Kyle Johannsen. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SuperCoolTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
